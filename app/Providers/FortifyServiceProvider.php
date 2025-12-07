f5265b83b56a70612c085cb4aee5a8b06f8e672e
Maman <rohman.banana27@gmail.com>
2025-12-07 23:01:58 +0700

    feat: add user registration functionality with username validation

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -43,6 +43,10 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.login');
         });
 
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
+
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
